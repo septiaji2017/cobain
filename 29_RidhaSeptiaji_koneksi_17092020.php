7a36c17af8718db3cd0fe1d9e338bc419db36720
septiaji2017 <septiaji2017@gmail.com>
2020-10-19 09:53:00 +0700

    Tambah Coba

---
+++ 29_RidhaSeptiaji_koneksi_17092020.php
@@ -0,0 +1,20 @@
+<?php
+	$host = 
+	mysqli_connect("localhost","root","");
+
+	if($host){
+		echo "koneksi host berhasil.<br/>";
+	}
+	else{
+		echo "koneksi gagal.<br/>";
+	}
+
+	$db = mysqli_select_db($host,"ppdb");
+
+	if($db){
+		echo "koneksi database berhasil.";
+	}
+	else{
+		echo "koneksi database gagal.";
+	}
+?>
